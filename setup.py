1c13eae24c29f303757b7cb1687897138d2b109f
saritha <saritha.jce@gmail.com>
2024-10-28 15:17:39 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages 
+
+setup(
+    name = "src",
+    version= "0.0.1",
+    description= "This is project for Wine Quality prediction",
+    author= "Saritha Miryala",
+    packages=find_packages(),
+    license="MIT"
+)
